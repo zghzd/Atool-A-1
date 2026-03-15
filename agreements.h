c2f6cedd596537b5a29339a7b5defbb286abdd2c
z z <zfg_0362@outlook.com>
2026-03-15 19:48:25 +0800

    一些小更改

--- agreements.h
+++ agreements.h
@@ -1,7 +1,14 @@
 #pragma once
 #include "External_library.h"
 #define ATOOL_FILE_HEAD "Atool,atf;"//file head
+#define ATOOL_FILE_EXT "atf"
 
 #ifndef SL_LIB
 #error no SL_lib find
 #endif
+
+/*scene file:所有内容在一行，且必须是第一行。此处为示例方便，分行。
+Atool,atf;
+scene;
+
+*/
--- doc-sky_candle.md
+++ doc-sky_candle.md
@@ -1,1 +1,4 @@
-﻿
+﻿# Atool-A-1:sky_candle
+Atool-A-1:sky_candle是sky_candle功能的完整名称。  
+
+# 
